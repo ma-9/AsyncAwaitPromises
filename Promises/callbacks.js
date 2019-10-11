d1bd2c9f047a0a6666e0aa5c4dd86e62cd9b4408
Manav Oza <manavoza99@gmail.com>
2019-10-11 17:31:05 +0530

    First Commit on Async Await Code

---
+++ Promises/callbacks.js
@@ -0,0 +1,28 @@
+const posts = [
+    {title: 'One Post', Description: 'This is One Post'},
+    {title: 'Second Post', Description: 'This is Second Post'}
+]
+
+function getPost() {
+    setTimeout(()=>{
+        let Result = '';
+        posts.forEach((post,index) => {
+            Result += `<li>${post.title}</li>`;
+        })
+        document.body.innerHTML = Result;
+    }, 1000)
+}
+
+// getPost();
+
+function createPost(newPost, callbacks) {
+    setTimeout(() => {
+        posts.push(newPost);
+        callbacks();
+    }, 2000)
+}
+
+createPost(
+    {title: 'Third Post', Description: 'This is Third Post', newID: 'aasa'},
+    getPost
+)
